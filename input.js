5974becde3467b5275d38d61694505046a4c3318
Artur Iatsko <arturyatsko@gmail.com>
2020-02-12 22:21:53 +0000

    Finished controls

--- input.js
+++ input.js
@@ -1,10 +1,25 @@
+let connection;
+
 const handleUserInput = (input) => {
   if (input === '\\q\n' || input === '\u0003') {
     process.exit();
   }
+  if (input === 'w' || input === 'W') {
+    connection.write("Move: up");
+  }
+  if (input === 'a' || input === 'A') {
+    connection.write("Move: left");
+  }
+  if (input === 's' || input === 'S') {
+    connection.write("Move: down");
+  }
+  if (input === 'd' || input === 'D') {
+    connection.write("Move: right");
+  }
 };
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
