267cb5361a7acee1d9fe4a69d6682b51356a2efe
Artur Iatsko <arturyatsko@gmail.com>
2020-02-12 21:59:24 +0000

    Added inout handling

--- play.js
+++ play.js
@@ -1,3 +1,27 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const handleUserInput = (input) => {
+  if (input === '\\q\n' || input === '\u0003') {
+    process.exit();
+  }
+}
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (data) => {
+    handleUserInput(data);
+  });
+  return stdin;
+}
+
+
+setupInput();
